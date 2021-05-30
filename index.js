2fd475ef03b49fd8b8a9e48a86475828e2d83912
cjope <craigjope@gmail.com>
2021-05-29 18:23:12 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById('input')
+
 function addingEventListener() {
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    return input.addEventListener('click',clickAlert);
 }
